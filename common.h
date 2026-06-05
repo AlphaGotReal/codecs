37a1f3a7f4c81725c0f3a9f9b128173d7d60b7c2
AlphaGotReal <subhampatra2411@gmail.com>
2026-06-06 04:29:03 +0530

    added utilities in common.h

--- common.h
+++ common.h
@@ -1,4 +1,32 @@
 #ifndef COMMON_H
 #define COMMON_H
+
+#include <time.h>
+
+double get_wall_time() {
+  struct timespec time;
+  clock_gettime(CLOCK_MONOTONIC, &time);
+  return (double)time.tv_sec + (double)time.tv_nsec * 1e-9;
+}
+
+#define cnow (clock() * 1e-6)
+#define rnow get_wall_time()
+
+#include <unistd.h>
+
+long get_file_size(const char *filename) {
+  FILE *file = fopen(filename, "rb");
+  if (file == NULL) {
+    return -1;
+  }
+
+  fseek(file, 0, SEEK_END);
+  long size = ftell(file);
+  fclose(file);
+
+  return size;
+}
+
 #define call(this, F, ...) (this)->F((this), ##__VA_ARGS__)
+
 #endif
