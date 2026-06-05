59b52bbb306e2d360255f3fedc527dd4f0e81551
AlphaGotReal <subhampatra2411@gmail.com>
2026-06-06 04:29:33 +0530

    trivial

--- common.h
+++ common.h
@@ -6,7 +6,7 @@
 double get_wall_time() {
   struct timespec time;
   clock_gettime(CLOCK_MONOTONIC, &time);
-  return (double)time.tv_sec + (double)time.tv_nsec * 1e-9;
+  return (double) time.tv_sec + (double) time.tv_nsec * 1e-9;
 }
 
 #define cnow (clock() * 1e-6)
